ef0cb97d55a3fc21f170bc30cc226f9fa420ba08
alysiacdennis <76969488+alysiacdennis@users.noreply.github.com>
2021-02-12 18:23:45 -0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+    <header>
+    
+       <div id="logo"><img class="main-logo" src="img/logo.jpg"></div>
+    
+    
+    
+     <nav>
+         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i> </a>
+         
+            <ul id="myLinks">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">FAQ's</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Registration</a></li> 
+            </ul>
+            
+        
+    </nav>
+        
+         </header>
---
+++ template.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="utf-8">        
+        <meta name="description" content="CHANGE-ME">
+        
+                
+        <title>Template</title>
+        <link href="style/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       
+        
+    </head>
+    
+        
+    <?php include 'header.php';?>
+
+    <main>    
+        <section class="container">
+            <article class="item">
+                <h1>Main Heading</h1>
+                <p>Sample text goes here.</p>
+                
+            </article>
+        
+        </section>
+        
+        <section class="container">
+            <article class="item">
+                <h2>Secondary Heading</h2>
+                <p>Sample text for secondary heading.</p>
+            </article>
+            <article class="item">
+                <h2>Secondary Heading</h2>
+                <p>Sample text for secondary heading.</p>
+            </article>
+        
+        </section>
+        
+        
+        
+    </main>
+    
+    <?php include 'footer.php'; ?>
+  
+    
+</html>
