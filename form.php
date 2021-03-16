4654fe3a3c6d9966f417049cffe60b9e7941b530
alysiacdennis <76969488+alysiacdennis@users.noreply.github.com>
2021-03-15 20:53:06 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <meta charset="utf-8">
+
+
+    <title>Registration</title>
+
+    <link href="style/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.typekit.net/esi3xon.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+
+
+</head>
+
+
+<?php include 'header.php';?>
+
+<main>
+
 <?php
     
 $servername = "localhost";
@@ -13,10 +38,20 @@ $emergencycontact = $_POST['emergencycontactname'];
 $emergencycontact_phone = $_POST['emergencycontactnumber'];
 $gender = $_POST['gender'];
 $size = $_POST['size'];
-$saturdayevents = $_POST['saturdayevents'];
-$sundayevents = $_POST['sundayevents'];
+$saturdayevents = $_POST['saturday'];
+$sundayevents = $_POST['sunday'];
+$chk="";
 $special_requests = $_POST['specialrequests'];
 
+foreach($saturdayevents as $chk1)  
+   {  
+      $chk .= $chk1.",";  
+   }  
+   
+foreach($sundayevents as $chk2)
+    {
+        $chk3 .= $chk2.",";
+    }
     
     // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -27,18 +62,25 @@ if ($conn->connect_error) {
     
 
 $sql ="
-  INSERT INTO eventregistration (name, age, roletype, phone, email, emergencycontactname, emergencycontactnumber, gender, size, saturdayevents, sundayevents, specialrequests ) VALUES ('$users_name', '$users_age', '$users_role', '$users_phone', '$users_email', '$emergencycontact', '$emergencycontact_phone', '$gender', '$size', '$saturdayevents', '$sundayevents', '$special_requests')";
+  INSERT INTO eventregistration (name, age, roletype, phone, email, emergencycontactname, emergencycontactnumber, gender, size, saturdayevents, sundayevents, specialrequests ) VALUES ('$users_name', '$users_age', '$users_role', '$users_phone', '$users_email', '$emergencycontact', '$emergencycontact_phone', '$gender', '$size', '$chk', '$chk3', '$special_requests')";
 
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  echo "<center><h1>Your registration was successful!</h1><br>If we have any questions, we will contact you via the information you provided.</b></p></center>";
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "<p>Uh oh. It looks like that form didn't go through. Please try again!</p> " . $sql . "<br>" . $conn->error;
 }
 
 
 $conn->close();
 
 ?>
+
+    </main>
+    
+    <?php include 'footer.php'; ?>
+
+
+</html>
 
 
